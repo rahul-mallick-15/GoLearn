17ef3d6f6c0724e157f6321d91c4f1d55c8b6a9e
Rahul Mallick <rahulmallick1115@gmail.com>
2025-02-18 23:45:43 +0530

    array of data operations

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	printBasicDataTypes()
+	// printBasicDataTypes()
+	arrayOfData()
 }
 
 func printBasicDataTypes() {
@@ -17,5 +18,24 @@ func printBasicDataTypes() {
 	fmt.Printf("char: %c", character)
 
 	var string string = "a string value"
-	fmt.Printf("\nstring: %s", string)
+	fmt.Printf("\nstring: %s\n", string)
+
+	fmt.Println("%T, %T, %T, %T", number, boolean, character, string)
+	fmt.Println(character)
+	fmt.Printf("%T, %T, %T, %T", number, boolean, character, string)
+}
+
+func arrayOfData() {
+	var charArray1 []rune = []rune{}
+	fmt.Println("Uninitialized Rune: ", charArray1, "len: ", len(charArray1), "capacity: ", cap(charArray1))
+
+	var charArray2 []rune = make([]rune, 2, 3)
+	fmt.Println("Rune Array of size : ", charArray2, "len: ", len(charArray2), "capacity: ", cap(charArray2))
+
+	var charArray3 []rune = []rune{'a', 98}
+	fmt.Println("Initialized Rune: ", charArray3, "len: ", len(charArray3), "capacity: ", cap(charArray3))
+	fmt.Printf("rune[%d] = %c, rune[%d] = %c\n", 0, charArray3[0], 1, charArray3[1])
+	fmt.Printf("rune[%d] = %d, rune[%d] = %d\n", 0, charArray3[0], 1, charArray3[1])
+	fmt.Printf("rune = %s\n", charArray3)
+	fmt.Printf("rune = %s", string(charArray3))
 }
