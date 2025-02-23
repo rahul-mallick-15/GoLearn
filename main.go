2b53a084b468bfb8d018a06b20860914b64c5417
Rahul Mallick <rahulmallick1115@gmail.com>
2025-02-23 23:56:24 +0530

    take input from stdin

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+)
 
 func main() {
 	// printBasicDataTypes()
 	// arrayOfData()
-	iterateArray()
+	// iterateArray()
+	userInput()
+	// menu()
 }
 
 func printBasicDataTypes() {
@@ -57,4 +65,81 @@ func iterateArray() {
 		fmt.Printf("rune[%d] = %c, %d\n", i, char, char)
 	}
 
+}
+
+func userInput() {
+	var number = int(5)
+	fmt.Printf(`Enter integer for address:%%d: %d
+			  %%v: %v
+			  %%p: %p
+ 			  %%x: %x
+			 `, &number, &number, &number, &number)
+	fmt.Scanf("%d", &number)
+	fmt.Printf("User input is: %d", number)
+
+	var reader = bufio.NewReader(os.Stdin)
+	fmt.Printf("\nType of reader: %T", reader)
+	reader.ReadString('\n')
+
+	char := 'A'
+	fmt.Printf("\nEnter char input: ")
+	b, _ := reader.ReadByte()
+	char = rune(b)
+	fmt.Printf("Entered char is %c, %v, %v", char, char, &char)
+
+	var str string = "myString value"
+	fmt.Printf("\nEnter input for string: ")
+	reader.ReadString('\n')
+	str, _ = reader.ReadString('\n')
+	fmt.Printf("Entered string: %s", str)
+
+	array := []int{1, 2, 3, 4, 5}
+	fmt.Println("Enter elements for array")
+	for i := range array {
+		fmt.Scanf("%d", &array[i])
+	}
+
+	fmt.Println("Enter elements:")
+	for _, val := range array {
+		fmt.Printf("%d ", val)
+	}
+	fmt.Println()
+}
+
+func clearScreen() {
+	// fmt.Print("\033[H\033[2J")
+	switch runtime.GOOS {
+	case "windows":
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	default:
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
+
+func menu() {
+	var check bool = true
+	var optionEntered rune
+	reader := bufio.NewReader(os.Stdin)
+	for check {
+		fmt.Printf("1.Enter 'y' to clear screen\n" +
+			"2.Enter 'h' to print 'Hello World'\n" +
+			"3.Enter 'q' to quit\n")
+		b, _ := reader.ReadByte()
+		reader.ReadString('\n')
+		optionEntered = rune(b)
+		switch optionEntered {
+		case 'y':
+			fmt.Printf("\033[H\033[2J")
+		case 'h':
+			fmt.Printf("Hello World\n")
+		case 'q':
+			check = false
+		default:
+			fmt.Println("Invalid option")
+		}
+	}
 }
