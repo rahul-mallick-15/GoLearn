a0085a8138d754610f2d1d3ee79b5667ba6c77d1
Rahul Mallick <rahulmallick1115@gmail.com>
2025-03-28 01:36:15 +0530

    command line contact book

--- Assignment/assignment.go
+++ Assignment/assignment.go
@@ -3,60 +3,164 @@ package Assignment
 import (
 	"bufio"
 	"fmt"
-	"math/rand"
 	"os"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 /* Objective:
-Build a CLI-based number guessing game where the program generates a random number, and the user has to guess it.
+Build a command-line Contact Book where users can add, search, and delete contacts.
 
 Requirements:
-Generate a random number between 1 and 100.
-Ask the user to guess the number.
-Respond if the guess is too high, too low, or correct.
-Keep track of the number of attempts.
-When the user guesses correctly, print a congratulatory message and the number of attempts.
+Add a Contact: Prompt the user for a name and a phone number. Store contacts in a map (map[string]string where name -> phoneNumber).
+Search for a Contact :Allow users to enter a name and find the corresponding phone number. If the name doesn’t exist, display a message saying "Contact not found".
+Delete a Contact: Allow users to remove a contact by name.
+List All Contacts: Print all saved contacts in a readable format.
+Exit the Program: Provide an option to exit the program.
 */
 
+var contacts map[string]string
+
 func Task() {
-	randomNumber, attempts := generateRandInRange(1, 100), 0
-	reader := bufio.NewReader(os.Stdin)
+	contacts = make(map[string]string)
 	for {
-		var guess int
-		var inputLine string
+		fmt.Printf("1.Search\t2.Add\t3.Delete\t4.List\t5.Exit\n")
+		fmt.Printf("Enter choice: ")
 
-		fmt.Printf("Guess random number between 1 and 100: ")
+		option := takeOptionInput()
 
-		inputLine, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Error reading input")
-			reader.Reset(os.Stdin)
-			continue
-		}
-
-		inputLine = strings.TrimSpace(inputLine)
-
-		guess, err = strconv.Atoi(inputLine)
-		if err != nil {
-			fmt.Println("Please enter a valid number")
-			continue
-		}
-
-		attempts++
-		if guess == randomNumber {
-			fmt.Printf("Congratulations, you guessed correctly in %d attempts\n", attempts)
-			break
-		} else if guess < randomNumber {
-			fmt.Printf("guessed number is lower\n")
-		} else {
-			fmt.Printf("guessed number is higher\n")
+		switch option {
+		case 1:
+			searchContact()
+		case 2:
+			addNewContact()
+		case 3:
+			deleteContact()
+		case 4:
+			listAllContacts()
+		case 5:
+			fmt.Printf("Exiting Program.")
+			os.Exit(0)
+		default:
+			fmt.Printf("Please enter valid option!\n")
 		}
 	}
-
 }
 
-func generateRandInRange(low int, high int) int {
-	return rand.Intn(high-low+1) + low
+func searchContact() {
+	if len(contacts) == 0 {
+		fmt.Printf("No contacts to search\n")
+		return
+	}
+	fmt.Printf("Enter contact name: ")
+	name := takeNameInput()
+	if _, exists := contacts[name]; !exists {
+		fmt.Printf("No contacts found with name='%s'\n", name)
+		return
+	}
+	fmt.Printf("Found contact {name: '%s', number:'%s'}\n", name, contacts[name])
+}
+
+func addNewContact() {
+	fmt.Printf("Enter contact name: ")
+	name := takeNameInput()
+	fmt.Printf("Enter contact number: ")
+	number := takeNumberInput()
+	contacts[name] = number
+	fmt.Printf("Added new contact {name: '%s', number: '%s'}\n", name, number)
+}
+
+func deleteContact() {
+	if len(contacts) == 0 {
+		fmt.Printf("No contacts to delete\n")
+		return
+	}
+	fmt.Printf("Enter contact name to delete: ")
+	name := takeNameInput()
+	if _, exists := contacts[name]; !exists {
+		fmt.Printf("Contact '%s' not found\n", name)
+		return
+	}
+	delete(contacts, name)
+	fmt.Printf("Contact '%s' successfully deleted\n", name)
+}
+
+func listAllContacts() {
+	i := 0
+	if len(contacts) == 0 {
+		fmt.Printf("Contacts list is empty\n")
+		return
+	}
+	for name, number := range contacts {
+		i += 1
+		fmt.Printf("[%d] name=%s\tnumber=%s\n", i, name, number)
+	}
+}
+
+func takeNumberInput() string {
+	for {
+		input := getInputFromUser()
+		if isValidContactNumber(input) {
+			return input
+		}
+		fmt.Println("Please enter a valid contact number (exactly 10 digits)")
+	}
+}
+
+func isValidContactNumber(number string) bool {
+	if len(number) != 10 {
+		return false
+	}
+
+	for _, char := range number {
+		if !unicode.IsDigit(char) {
+			return false
+		}
+	}
+	return true
+}
+
+func takeNameInput() string {
+	for {
+		input := getInputFromUser()
+		if isValidName(input) {
+			return input
+		}
+		fmt.Println("Please enter a valid name (letters only)")
+	}
+}
+
+func isValidName(name string) bool {
+	if len(name) == 0 {
+		return false
+	}
+
+	for _, char := range name {
+		if !unicode.IsLetter(char) && char != ' ' && char != '-' {
+			return false
+		}
+	}
+	return true
+}
+
+func takeOptionInput() int {
+	input := getInputFromUser()
+
+	option, err := strconv.Atoi(input)
+	if err != nil {
+		return -1
+	}
+	return option
+}
+
+func getInputFromUser() string {
+	reader := bufio.NewReader(os.Stdin)
+	inputLine, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Printf("Error reading input")
+		reader.Reset(os.Stdin)
+		return ""
+	}
+	return strings.TrimSpace(inputLine)
 }
